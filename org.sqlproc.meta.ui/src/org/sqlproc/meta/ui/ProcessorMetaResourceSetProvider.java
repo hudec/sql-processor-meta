d3948a101a5f7ae287c82e9274d7596dd64e5481
Vladimír Hudec <Vladimir.Hudec@gmail.com>
2015-03-02 05:31:50 +0100

    Issue #2: Grammar 2.1

--- org.sqlproc.meta.ui/src/org/sqlproc/meta/ui/ProcessorMetaResourceSetProvider.java
+++ org.sqlproc.meta.ui/src/org/sqlproc/meta/ui/ProcessorMetaResourceSetProvider.java
@@ -1,14 +1,17 @@
 package org.sqlproc.meta.ui;
 
+import org.apache.log4j.Logger;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.xtext.ui.resource.XtextResourceSetProvider;
 
 public class ProcessorMetaResourceSetProvider extends XtextResourceSetProvider {
 
+    protected Logger LOGGER = Logger.getLogger(ProcessorMetaResourceSetProvider.class);
+
     @Override
     public ResourceSet get(IProject project) {
-        System.out.println(project);
+        LOGGER.info("Openning " + project + "...");
         ResourceSet set = super.get(project);
         // System.out.println(set);
         return set;
